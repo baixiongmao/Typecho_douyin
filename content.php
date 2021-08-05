a7786ee7dc7c36bd8499dec6ebd2fadee65a7bce
baixiongmao <zxj417870308@gmail.com>
2021-08-06 00:36:41 +0800

    图片增加懒加载

--- content.php
+++ content.php
@@ -8,7 +8,7 @@
    preg_match_all($reg, $fullcontent, $matches);
    $countPostImg = count($matches[0]);
    for ($k=0; $k < $countPostImg; $k++) { 
-   echo ' <div class="swiper-slide "><img src="<?php echo $this->options->themeUrl ?>/img/loading.gif" class="swiper-lazy" data-src="'.$matches[0][$k].'" alt="'.$this->title.'"/></div>';}
+   echo ' <div class="swiper-slide "><img src="/usr/themes/Typecho_douyin/img/loading.gif" class="swiper-lazy" data-src="'.$matches[0][$k].'" alt="'.$this->title.'"/></div>';}
    ?>
  </div>  
  <div class="swiper-pagination"></div>
